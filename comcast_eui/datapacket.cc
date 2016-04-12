ddd3b0408da3a6feed2698bee074c2501056ec7d
HanEui <euiseonghan@gmail.com>
2016-04-11 23:56:11 -0400

    deleted duplicate

--- comcast_eui/datapacket.cc
+++
@@ -1,9 +0,0 @@
-#include "datapacket.h"
-
-DataPacket(std::string id, node* src, node* dst, flow* fl, bool timeout){
-  id_ = id;
-  origin_ = src;
-  dest_ = dst;
-  flow_ = fl;
-  timeout_ = timeout;
-}
