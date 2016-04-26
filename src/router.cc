1a3aa3039e8213b2b0509083dec423ecfb8e98ea
HanEui <euiseonghan@gmail.com>
2016-04-25 20:32:22 -0400

    router update

--- src/router.cc
+++ src/router.cc
@@ -19,3 +19,30 @@ void Router::RecievePacket(const Packet &p, const double time){
 bool Router::allowedToTransmit(){
   return links_[0].isAvailable();
 }
+
+//separate vectors for router and host?
+void UpdateTable(){
+  UpdateCostVec();
+  for router in routers{
+    for routing_table[router_id]
+      double temp = routing_table[router_id]->first() + cost_vector(router_id);
+      if (temp < routing_table[this]) 
+        routing_table[this] = temp;
+  }
+}
+
+void UpdateCostVec(){ // updates cost vector every time step
+  for l in link{
+    cost_vector[i] = l.getCost(); // sum (packetSize*rate )
+  }
+}
+
+Event SendControlEvent(control packet){
+  //sends its distance vector to its neighbors
+}
+
+Event ReceiveControlEvent(control packet){
+  routing_table[packet.getSrc().id()] = packet.getSrc().distanceVec ;
+}
+
+//initiate with greedy algorithm
--- src/router.h
+++ src/router.h
@@ -2,6 +2,7 @@
 #define ROUTER_H_
 
 #include "node.h"
+class Network;
 class Packet;
 
 class Router: public Node{
@@ -10,5 +11,10 @@ class Router: public Node{
     void SendPacket(const Packet &p, const double time);
     void RecievePacket(const Packet &p, const double time);
     bool allowedToTransmit();
+    link& GetRoute(Packet&); // looks up the routing table and returns the link
+    void UpdateTable(); // updates the routing table every x time step
+  private:
+    std::unordered_map< std::string /*router id */, std::vector< std::pair<int /*distance*/, std::string /*link to next hop*/> > > routing_table;
+    std::vector<int> cost_vector;
 };
 #endif
