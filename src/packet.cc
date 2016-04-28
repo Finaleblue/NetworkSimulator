2e13ea847e8d56bfac32ebadd03a64cafb4e380e
HanEui <euiseonghan@gmail.com>
2016-04-28 10:08:30 -0400

    link compiles

--- src/packet.cc
+++ src/packet.cc
@@ -7,42 +7,46 @@
 #include "packet.h"
 #include "global.h"
 
-Packet::Packet(const std::string id, const Node& src, const Node& dst):
-  id_(id),
-  type_(id[0]),
-  origin_(src),
-  dest_(dst),
-  size_(global::PacketSize(type_)),
-  seq_(std::stoi(id.substr(1))){}
+Packet::Packet(std::string id, const Node& src, const Node& dst) :
+  id_(id), type_(ToType(id)), src_(src), seq_(ToSeq(id)), size_(global::PacketSize(type_)), dst_(dst)
+{}
 
-Packet::Packet(const std::string type, int seq, const Node& src, const Node& dst):
-  id_(type + std::to_string(seq)),
-  type_(type),
-  seq_(seq),
-  src_(src),
-  size_(global::PacketSize(type_)),
-  dst_(dst){}
+Packet::Packet(std::string type, int seq, const Node& src, const Node& dst):
+  id_(type + std::to_string(seq)), type_(type), seq_(seq), src_(src), size_(global::PacketSize(type_)), dst_(dst)
+{}
 
-Node& Packet::GetSrc() const{
+const Node& Packet::GetSrc() const{
   return src_;
 }
 
-std::string id() const{
+const std::string Packet::id() const{
   return id_;
 }
 
-Node& Packet::GetDest() const{
-  return dest_;
+const Node& Packet::GetDst() const{
+  return dst_;
 }
 
-int Packet::size() const{
+const int Packet::size() const{
   return size_;
 }
 
-int Packet::seqNum() const{
-  return seq_num_;
+const int Packet::seqNum() const{
+  return seq_;
 }
 
-std::string Packet::type() const{
+const std::string Packet::type() const{
   return type_;
+}
+
+std::string Packet::ToType(std::string id){
+  std::stringstream ss;
+  ss<<id[0];
+  std::string s;
+  ss>>s;
+  return s;
+}
+
+int Packet::ToSeq(std::string id){
+  return id[1]-'0';
 }
